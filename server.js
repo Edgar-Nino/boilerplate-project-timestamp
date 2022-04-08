39c763bf0ed5a221ad010dd5cc2ee2412139b61d
Edgar Niño <edgarnio@live.com>
2022-04-08 13:58:26 -0500

    Fixed a bug with the date

--- server.js
+++ server.js
@@ -3,39 +3,49 @@
 
 // init project
 var express = require('express');
-require('dotenv').config()
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get("/", function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get("/api/:date", function(req, res) {
 
-app.get("/api/:date", function (req, res) {
-  const reqDate = req.params.date;
-  let date = (isNaN(reqDate))?reqDate:reqDate*1;
-  date = new Date(date);
-  
-  res.json({unix: date.getTime(), utc: date.toUTCString()})
+  try {
+    const reqDate = req.params.date;
+    let date = (isNaN(reqDate)) ? reqDate : reqDate * 1;
+    date = new Date(date);
+    if (isNaN(date)) { throw "Invalid Date" }
+    res.json({ unix: date.getTime(), utc: date.toUTCString() })
+  }
+  catch (e) {
+    res.json({ error: e })
+  }
 })
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+app.get("/api", function(req, res) {
+
+  try {
+    const date = new Date(Date.now());
+    res.json({ unix: date.getTime(), utc: date.toUTCString() })
+  }
+  catch (e) {
+    res.json({ error: e })
+  }
+})
 
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
